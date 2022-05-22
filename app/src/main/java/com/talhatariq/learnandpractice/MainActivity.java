b3950f6020a9fb5955e4921244a063d4262af281
Talha Tariq <talhacoder01@gmail.com>
2022-05-22 09:45:31 +0500

    Contribute Page Done

---
+++ app/src/main/java/com/talhatariq/learnandpractice/MainActivity.java
@@ -0,0 +1,30 @@
+    package com.talhatariq.learnandpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+    public class MainActivity extends AppCompatActivity {
+
+    Button learn, practice, contribute;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        learn = (Button) findViewById(R.id.question);
+        practice = (Button) findViewById(R.id.practice);
+        contribute = (Button) findViewById(R.id.contribute);
+
+        //On contribute Click Listener
+        contribute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+            }
+        });
+
+    }
+}
